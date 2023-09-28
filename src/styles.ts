93f7d267215c573464636f2b76ae93e46a646586
NatáliaFonseca <fonsecanatalianf@gmail.com>
2023-09-28 15:38:40 -0300

    fix: implementing styled-components

--- src/styles.ts
+++ src/styles.ts
@@ -1,3 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+
 :root {
   --cor-principal: #a7727d;
   --cor-secundaria: #f9f5e7;
@@ -14,15 +19,15 @@
 body {
   padding-bottom: 120px;
 }
+`
+export default EstiloGlobal
 
-.container {
+export const Container = styled.div`
   max-width: 1024px;
   width: 100%;
   margin: 0 auto;
-}
 
-@media (max-width: 1024px) {
-  .container {
+  @media (max-width: 1024px) {
     max-width: 80%;
   }
-}
+`
